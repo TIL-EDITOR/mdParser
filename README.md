10ce3d96216f4ee8c1d66a0fd33cb154d6eda273
ChangHa Lee <dlckdgk4858@gmail.com>
2022-08-17 00:09:20 +0900

    update
    Co-authored-by: Youngjun Choi <choi2021@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mdParser
 markdown 을 json 형식으로 파싱한다. 커스터마이징을 위해 처음부터 직접 구현해본다.
+
+# head 1
+paragraph
+## head 2
+### head 3
+paragraph
---
+++ src/normalizer.js
@@ -0,0 +1,8 @@
+// 1. md 파일을 정규화한다.
+// rule 에 맞게끔 md파일을 수정하는 것!
+// ex. 실제론 한 줄 짜린데, 파일엔 두 줄로 되어있음.
+/**
+ * # heading 1 
+ * para para para para para para para para para para para 
+ * para para para para para para para para para para para 
+ */
