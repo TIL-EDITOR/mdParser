4f8adbe7a50725b218f8e5c49f08b92086b95e91
gurunelee <dlckdgk4858@gmail.com>
2022-08-21 21:09:21 +0900

    FEAT normalizer 개행문자 축소 구현

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // 0. 라이브러리를 시행한다.
+const normalizer = require('./src/normalizer')
+
+exports.toJson = {
+    do(data) {
+        const normalizedData = normalizer.normalize(data)
+        return normalizedData
+    }
+}
--- src/normalizer.js
+++ src/normalizer.js
@@ -1,8 +1,21 @@
 // 1. md 파일을 정규화한다.
-// rule 에 맞게끔 md파일을 수정하는 것!
-// ex. 실제론 한 줄 짜린데, 파일엔 두 줄로 되어있음.
+// 연속 두 번 이상의 개행을 한 번으로 바꿈
+// 마지막 줄의 개행문자 이후 공백 삭제 (마지막 개행 포함)
 /**
- * # heading 1 
- * para para para para para para para para para para para 
- * para para para para para para para para para para para 
+ *  # head1
+    ## head2 
+    paragraph with no new-line
+    this line belong to a first line\
+    paragraph with no new-line
+    this line belong to a first line\
+    this line is new paragraph\
+    this line is new paragraph too\
  */
+// \n -> !
+const regex = /(\n){2,}/g
+// regex.match
+
+exports.normalize = function (data) {
+  const normalized_str=data.replaceAll(regex,"\n")
+  return normalized_str
+}
