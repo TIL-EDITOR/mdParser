fc71987da37927f1e13412fe19f0290ea918862e
gurunelee <dlckdgk4858@gmail.com>
2022-08-21 21:08:45 +0900

    test 코드
    Co-authored-by: Youngjun Choi <choi2021@users.noreply.github.com>

---
+++ test/mdloader.js
@@ -0,0 +1,43 @@
+// heading, text, 개행문자
+// 를 js 객체로 바꿔보자
+
+const path = require('path')
+const fs = require('fs');
+const { toJson } = require('../index') // 우리가 만들거임
+
+let data = null;
+try {
+  data = fs.readFileSync(
+    path.resolve('./test/test.md'), 'utf8'
+  );
+} catch(err) {
+  console.log(err)
+}
+console.log(data)
+
+try {
+  const resultJson = toJson.do(data)
+  console.log('\n');
+  console.log(resultJson)
+} catch (err) {
+  console.log(err)
+}
+
+/**
+ * [
+    {
+        "type": "head1",
+        "text": "head1"
+    },
+    ...
+    {
+        "type": "text",
+        "text": "paragraph with no new-line this line belong to a first line"
+    },
+    {
+        "type": "text",
+        "text": "paragraph with no new-line this line belong to a first line"
+    }
+    ...
+]
+ */
---
+++ test/test.md
@@ -0,0 +1,13 @@
+# head1
+## head2
+paragraph with no new-line\
+this line belong to a first line\
+
+paragraph with no new-line
+
+this line belong to a first line\
+this line is new paragraph\
+
+this line is new paragraph too\
+
+
